5ce59c4ad20d04d2f74723a8a91784b6351004e2
soulef77 <saoud.soulef@laposte.net>
2025-08-01 12:34:28 +0200

    ajout de fichiers version 4

---
+++ src/main/java/tp/com/example/project/Main.java
@@ -0,0 +1,17 @@
+package main.java.tp.com.example.project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+
+public String getMessage() {
+        return "Hello, Jenkins!";
+    }
+
+    public String getMessage(String customMessage) {
+        return customMessage;
+    }
+}
+    
+
