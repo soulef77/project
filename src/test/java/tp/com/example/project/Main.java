632ec6814462e4b905ebc4590abc3fba862d478c
soulef77 <saoud.soulef@laposte.net>
2025-08-01 15:15:52 +0200

    ajout de fichiers version 4

---
+++ src/test/java/tp/com/example/project/Main.java
@@ -0,0 +1,12 @@
+package test.java.tp.com.example.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
--- src/test/java/tp/com/example/project/MainTest.java
+++ src/test/java/tp/com/example/project/MainTest.java
@@ -1,6 +1,13 @@
 
-
+import tp.com.example.project.Main;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import tp.com.example.project.Main;
+import tp.com.example.project.Main;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 public class MainTest {
 
